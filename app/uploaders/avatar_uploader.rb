3e0469ad3af7214be544cae1c9315106dc7b6a7b
Kerron Streater <kerronstreater@gmail.com>
2017-07-20 19:48:13 +0000

    uploader try 2

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,7 +5,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.evn. production?
+  if Rails.evn.production?
     storage :file
   else
     storage :fog
